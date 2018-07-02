4843be11846633c0b10b5088c9c979cf5b477407
katol16 <karol.vogelgezang@gmail.com>
2018-07-02 21:14:51 +0200

    sticky socialMedia

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,26 @@
+// STICKY socialMedia
+$(function(){
+        // Check the initial Poistion of the Sticky Header
+        var stickyHeaderTop = $('.socialMedia').offset().top;
+        var stickyHeaderRight = $(window).width() - ($('.socialMedia').offset().left + $('.socialMedia').outerWidth());
+        var socialMediaWidth = document.querySelector('.socialMedia').offsetWidth;
+        console.log(socialMediaWidth);
+
+        $(window).scroll(function(){
+                if( $(window).scrollTop() > stickyHeaderTop ) {
+                        $('.socialMedia').css({position: 'fixed', width: socialMediaWidth+'px', top: '0px', right: stickyHeaderRight+'px', marginTop: '0px', height: 'auto'});
+                		$('.socialMedia--mainSite').css({paddingTop: '0px'});
+                } else {
+                        $('.socialMedia').css({position: 'static', marginTop: '20px', float: 'right'});
+                		$('.socialMedia--mainSite').css({paddingTop: '20px'});
+                }
+        });
+  });
+
+
 // GALERIA zdjęć
 var galleryBox = document.querySelector(".galleryBox"),
+	galleryBox__btn = document.querySelector(".galleryBox__btn"),
 	galleryImg = document.querySelector(".galleryImg"),
 	aside = document.querySelector("aside"),
 	arrowLeft = document.querySelector(".arrow-left"),
@@ -8,6 +29,7 @@ var galleryBox = document.querySelector(".galleryBox"),
 	numberOfPictures = 8;
 	close = document.querySelector(".fa-times");
 
+galleryBox__btn.addEventListener('click', showImgWithId);
 galleryBox.addEventListener('click', showImg);
 arrowLeft.addEventListener('click', prevImg);
 arrowRight.addEventListener('click', nextImg);
@@ -16,6 +38,12 @@ close.addEventListener('click', closeImg);
 function showImg(event) {
 	aside.style.display = "flex";
 	nr = event.target.id;
+	galleryImg.src="posts/norway/"+nr+".jpg";
+}
+
+function showImgWithId(event) {
+	aside.style.display = "flex";
+	nr = 1;
 	galleryImg.src="posts/norway/"+nr+".jpg";
 }
 
