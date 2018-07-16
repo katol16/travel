30818408c0239fe4e4519ae815f871fc5b5c7078
katol16 <karol.vogelgezang@gmail.com>
2018-07-16 22:04:29 +0200

    poprawione sticky socialMedia i dodany psot o Rumuni

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,33 +5,28 @@ $(window).resize(function() {
 
 });
 
+var postContainer = document.querySelector('.postContainer');
 
 // trzeba to zrobić za pomocą klas!
 function changeSocialMediaPosition() {
-	if ( ($(document).width() > 768) || ($(document).hasClass('.socialMedia--mainSite') && ($(document).width() > 1170))) {
-		$(function(){
-		    // Check the initial Poistion of the Sticky Header
-		    var stickyHeaderTop = $('.socialMedia').offset().top;
-		    var stickyHeaderRight = $(window).width() - ($('.socialMedia').offset().left + $('.socialMedia').outerWidth());
-		    var socialMediaWidth = document.querySelector('.socialMedia').offsetWidth;
+	$(function(){
+	    // Check the initial Poistion of the Sticky Header
+	    var stickyHeaderTop = $('.socialMedia').offset().top;
+	    var stickyHeaderRight = $(window).width() - ($('.socialMedia').offset().left + $('.socialMedia').outerWidth());
+	    var socialMediaWidth = document.querySelector('.socialMedia').offsetWidth;
 
-		    window.addEventListener('scroll', scrollFunc);
-		    function scrollFunc(){
-		    	console.log(stickyHeaderTop);
-		    	console.log($(window).scrollTop());
-		        if( ($(window).scrollTop() > stickyHeaderTop) && ($(document).width() > 768) ){
-		            $('.socialMedia').css({position: 'fixed', width: socialMediaWidth+'px', top: '0px', right: stickyHeaderRight+'px', marginTop: '0px', height: 'auto',});
-		            console.log("test");
-		        } else {
-		            $('.socialMedia').css({position: 'static', marginTop: '20px',top: 'none', width: ''});
-		        }
-		    };
-		});
-	}
-	
+	    document.addEventListener('scroll', scrollFunc);
+	    function scrollFunc(){
+	        if( ( $(window).scrollTop() > stickyHeaderTop) && ($(document).width() > 768) && ($('body').hasClass('postBody')) ||
+	         ( $(window).scrollTop() > stickyHeaderTop) && ($(document).width() > 1170) && $('.socialMedia').hasClass('socialMedia--mainSite') ) {
+	            $('.socialMedia').css({position: 'fixed', width: socialMediaWidth+'px', top: '0px', right: stickyHeaderRight+'px', marginTop: '0px', height: 'auto',});
+	        } else {
+	            $('.socialMedia').css({position: 'static', marginTop: '20px',top: 'none', width: ''});
+	        }
+	    };
+	});	
 }
 changeSocialMediaPosition();
-
 
 // GALERIA zdjęć
 var galleryBox = document.querySelector(".galleryBox"),
